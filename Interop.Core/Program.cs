c656b883cc9fefa2580e52560b9b9b24f1cc0ee1
MarceloMilbradt <milbradthz@gmail.com>
2023-02-07 21:00:35 -0300

    Initial commit

---
+++ Interop.Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+
+HttpClient httpClient = new HttpClient();
+Mensagem mensagem;
+
+if (args.Length > 0)
+{
+    var conteudo = args[0];
+    mensagem = new Mensagem(DateTime.Now, conteudo, Guid.NewGuid());
+}
+else
+{
+    mensagem = new Mensagem(DateTime.Now, "Nenhuma Mensagem!", Guid.NewGuid());
+}
+
+var response = httpClient.PostAsJsonAsync("https://projetosd-d46bb-default-rtdb.firebaseio.com/interop.json", mensagem);
+
+Console.WriteLine(response.Result);
+
+record Mensagem(DateTime DataHora, string Conteudo, Guid Id);
